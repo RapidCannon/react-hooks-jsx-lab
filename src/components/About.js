104bb6a057bf255a767be81367a9f8c3c3549f1b
RapidCannon <payton.logan@mail.com>
2022-12-28 17:01:08 -0600

    done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div>
+
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I am a front-end developer from San Antonio and I enjoy the table top game Warhammer 40k</p>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this"></img>
+    </div>
+
+  </div>;
 }
 
 export default About;
